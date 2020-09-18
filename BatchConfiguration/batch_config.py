32226fd7db9edfc482e45266e03067261dcd097e
Darwin Barredo <darwin.barredo9@gmail.com>
2020-09-18 21:08:54 +0800

    Create batch_config.py

---
+++ BatchConfiguration/batch_config.py
@@ -0,0 +1,33 @@
+IP_LIST = open('switch_ip')
+for IP in IP_LIST:
+    RTR = {
+        'device_type': 'cisco_ios',
+        'ip':   IP,
+        'username': 'admin',
+        'password': 'admin',
+    }
+
+    print ('\nConnecting to the Switch ' + IP.strip() + ' \n')
+    try:
+        net_connect = ConnectHandler(**RTR)
+    except NetMikoTimeoutException:
+        print ('Device not reachable' )
+        continue
+
+    except NetMikoAuthenticationException:
+        print ('Authentication Failure' )
+        continue
+
+    except SSHException:
+        print ('Make sure SSH is enabled' )
+        continue
+
+    output = net_connect.send_config_from_file(config_file='18_switch_config')
+    print(output)
+
+    print('\n Saving the Switch configuration \n')
+    output = net_connect.save_config()
+    print(output)
+
+    output = net_connect.send_command('show ip route')
+    print(output)
